40f546dfc274f11daee93e3f0abc39b9341a1437
marin postma <postma.marin@protonmail.com>
2019-08-07 22:58:08 +0200

    can load ROM

--- src/ram.rs
+++ src/ram.rs
@@ -18,6 +18,11 @@ impl RAM {
     pub fn write_byte(&mut self, position: usize, value: u8) {
         self.mem[position] = value
     }
+
+    pub fn mem_cpy(&mut self, src: &[u8], offset: usize) {
+        let slice = &mut self.mem[offset..(offset + src.len())];
+        slice.clone_from_slice(src);
+    }
 }
 
 #[cfg(test)]
@@ -35,5 +40,18 @@ mod tests {
         assert_eq!(ram.read_byte(5), 0);
         ram.write_byte(42, 0x1f);
         assert_eq!(ram.read_byte(42), 0x1f);
+    }
+
+    #[test]
+    fn test_mem_cpy() {
+        let mut ram = RAM::new();
+        let data = [1, 2, 3, 4, 5];
+        let offset = 0x200;
+        assert_eq!(ram.read_byte(offset), 0);
+        ram.mem_cpy(&data, offset);
+        for i in offset..data.len() {
+            assert_eq!(ram.read_byte(offset + i), data[i]);
+        }
+        assert_eq!(ram.read_byte(offset - 1), 0);
     }
 }
